f2ce2a9deacc473f746cab294d66e25bb2d94243
joel <joel.uss1728@gmail.com>
2023-08-16 19:33:19 -0700

    apis login, logout y crear usuario

---
+++ backend/app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Auth\Events\Validated;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    
+    public function login(Request $request){
+        $user = User::whereName($request->name)->first();
+
+        if(!$user || !Hash::check($request->password, $user->password)){
+            return response()->json([
+                'message' => 'Ha ocurrido un error',
+                'code' => -1,
+                'data' => null
+            ]);
+        }
+
+        $user->token = $user->createToken('session-token')->plainTextToken;
+        return response()->json([
+            'message' => 'Registro consultado correctamente',
+            'code' => 1,
+            'data' => $user
+        ]);
+    }
+
+
+    public function logout(){
+        request()->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Sesion cerrada correctamente',
+            'code' => 1,
+            'data' => null
+        ]);
+    }
+
+    public function store(Request $request){
+    
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|unique:users,email',
+            'password' => 'required' 
+        ]);
+
+        if($validator->passes()){
+            $user = User::create($request->all());
+            return response()->json([
+                'message' => 'Registro creado correctamente',
+                'code' => 1,
+                'data' => $user
+            ]);
+        }
+
+
+        return response()->json([
+            'message' => 'Ha ocurrido un error',
+            'code' => -1,
+            'data' => $validator->errors()->all()
+        ]);
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\UserController;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,6 +21,21 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/user', function(){
-    return User::get();
+
+
+Route::controller(UserController::class)->prefix('/users')->group(static function(){
+    Route::post('/login', 'login');
+    Route::post('/', 'store');
+
+});
+
+Route::middleware('auth:sanctum')->group(function(){
+    Route::get('/user', function(){
+        return User::get();
+    });
+
+    Route::controller(UserController::class)->prefix('/users')->group(function(){
+        Route::get('/logout', 'logout');
+    });
+    
 });
